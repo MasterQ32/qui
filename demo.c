4f5145b9627bb58dda5458066b6e0823dab43f82
Felix Queißner <git@mq32.de>
2017-06-14 20:07:54 +0200

    Adds basic RPC.

--- demo.c
+++ demo.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <init.h>
+#include <rpc.h>
+
 int main(int argc, char ** argv)
 {
+	stdout = NULL; // Console only!
+	
 	printf("Hello, Client!\n");
+	pid_t svc;
+	while((svc = init_service_get("gui")) == 0) {
+		// ?
+	}
+	
+	uint32_t result = rpc_get_dword(svc, "WNCREATE", 0, NULL);
+	
+	printf("Got result: %u\n", result);
+	
 	return EXIT_SUCCESS;
 }
--- qui.c
+++ qui.c
@@ -11,10 +11,17 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 
+#include <rpc.h>
+#include <init.h>
+
 driver_context_t* graphics;
 video_bitmap_t *frontbuffer;
 
-void video_init();
+void svcWindowCreate(pid_t client, uint32_t correlation_id, size_t size, void *args)
+{
+	printf("Service got request!\n");
+	rpc_send_dword_response(client, correlation_id, 42);
+}
 
 int main(int argc, char** argv)
 {
@@ -39,84 +46,183 @@ int main(int argc, char** argv)
 		exit(EXIT_FAILURE);
 	}
 	
-	SDL_Surface * img = IMG_Load("window.gif");
-	if(img == NULL) {
+	SDL_Surface * skin = IMG_Load("skin.png");
+	if(skin == NULL) {
 		printf("Failed to open image: %s\n", IMG_GetError());
 		exit(EXIT_FAILURE);
 	}
 	
+	SDL_Surface * cursor = IMG_Load("cursor.png");
+	if(cursor == NULL) {
+		printf("Failed to open image: %s\n", IMG_GetError());
+		exit(EXIT_FAILURE);
+	}
+	
+	SDL_ShowCursor(SDL_DISABLE);
+	
+	init_service_register("gui");
+	
+	register_message_handler("WNCREATE", svcWindowCreate);
+	
 	SDL_Event e;
 	bool quit = false;
+	SDL_Rect target = {
+		16, 16,
+		240, 320
+	};
+	int mouseX = 0, mouseY = 0;
+	bool dragging = false;
 	while(!quit)
 	{
 		while(SDL_PollEvent(&e))
 		{
 			if(e.type == SDL_QUIT) quit = true;
 			if(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) quit = true;
+			
+			if(e.type == SDL_MOUSEBUTTONDOWN) {
+				printf("x,y=(%d,%d)\n", e.button.x, e.button.y);
+				dragging = (e.button.x >= target.x + 2)
+					&& (e.button.x <= target.x + target.w - 3)
+					&& (e.button.y >= target.y + 3)
+					&& (e.button.y <= target.y + 21);
+			}
+			if(e.type == SDL_MOUSEBUTTONUP) {
+				dragging = false;
+			}
+			if(e.type == SDL_MOUSEMOTION)
+			{
+				mouseX = e.motion.x;
+				mouseY = e.motion.y;
+				if(dragging) {
+					target.x += e.motion.xrel;
+					target.y += e.motion.yrel;
+				}
+			}
 		}
 		
+		// Clear screen
 		SDL_FillRect(window, NULL, SDL_MapRGB(window->format, 0, 0x80, 0x80));
 		
-		SDL_Rect rect = {
-			16, 16,
-			img->w, img->h
-		};
-		SDL_BlitSurface(img, NULL, window, &rect);
+		// Draw window!
+		{
+			SDL_Rect rect, src;
+			{
+				target.x -= 1;
+				target.y -= 1;
+				target.w += 2;
+				target.h += 2;
+				SDL_FillRect(window, &target, SDL_MapRGB(window->format, 0xFF, 0x00, 0x00));
+
+				target.x += 1;
+				target.y += 1;
+				target.w -= 2;
+				target.h -= 2;
+			}
+			SDL_FillRect(window, &target, SDL_MapRGB(window->format, 0xC0, 0xC0, 0xC0));
+			
+			src = (SDL_Rect) {
+				0, 0,
+				4, 22
+			};
+			rect = (SDL_Rect) {
+				target.x, target.y,
+				4, 22
+			};
+			SDL_BlitSurface(skin, &src, window, &rect);
+			
+			for(int i = 4; i < (target.w - 22); i += 22)
+			{
+				src = (SDL_Rect) {
+					4, 0,
+					22, 22
+				};
+				rect = (SDL_Rect) {
+					target.x + i, target.y,
+					22, 22
+				};
+				if(rect.x + rect.w > target.w - 22)
+				{
+					rect.w = target.w - i;
+				}
+				SDL_BlitSurface(skin, &src, window, &rect);
+				rect.y = target.y + target.h - 3;
+				src.y = 41;
+				SDL_BlitSurface(skin, &src, window, &rect);
+			}
+			
+			src = (SDL_Rect) {
+				34, 0,
+				22, 22
+			};
+			rect = (SDL_Rect) {
+				target.x + target.w - 22, target.y,
+				4, 22
+			};
+			SDL_BlitSurface(skin, &src, window, &rect);
+			
+			src = (SDL_Rect) {
+				0, 41,
+				4, 3
+			};
+			rect = (SDL_Rect) {
+				target.x, target.y + target.h - 3,
+				4, 3
+			};
+			SDL_BlitSurface(skin, &src, window, &rect);
+			
+			src = (SDL_Rect) {
+				34, 41,
+				22, 3
+			};
+			rect = (SDL_Rect) {
+				target.x + target.w - 22, target.y + target.h - 3,
+				4, 3
+			};
+			SDL_BlitSurface(skin, &src, window, &rect);
+			
+			
+			
+			
+			for(int i = 22; i < (target.h - 3); i += rect.h)
+			{
+				src = (SDL_Rect) {
+					0, 22,
+					3, 19
+				};
+				rect = (SDL_Rect) {
+					target.x, target.y + i,
+					src.w, src.h
+				};
+				if(rect.y + rect.h > (target.y + target.h - 3))
+				{
+					rect.h = target.h - 3 - i;
+					src.h = rect.h;
+				}
+				
+				src.x = 0;
+				rect.x = target.x;
+				SDL_BlitSurface(skin, &src, window, &rect);
+				src.x = 53;
+				rect.x = target.x + target.w - 3;
+				SDL_BlitSurface(skin, &src, window, &rect);
+			}
+		}
+		
+		SDL_BlitSurface(
+			cursor,
+			NULL,
+			window,
+			&((SDL_Rect){
+				mouseX, mouseY,
+				16, 24
+			}));
+		
+		// Render everything
 		SDL_Flip(window);
 		SDL_Delay(10);
 	}
 	
 	SDL_Quit();
 	
-	return;
-
-	while (1) {
-		// CLEAR
-		libvideo_change_color(0,0,0,0);
-		libvideo_draw_rectangle(0,0,1024,768);
-
-		// RENDER
-		for(int x = 0; x < 256; x++)
-		{
-			libvideo_change_color(0, x, 0, 0);
-			libvideo_draw_rectangle(x,0,1,768);
-			
-			libvideo_change_color(0, 255, x, 0);
-			libvideo_draw_rectangle(256+x,0,1,768);
-			libvideo_change_color(0, 255, 255, x);
-			libvideo_draw_rectangle(512+x,0,1,768);
-		}
-		
-		// SWAP
-		libvideo_do_command_buffer();
-		libvideo_flip_buffers(frontbuffer);
-	}
-	return 0;
-}
-
-void video_init()
-{
-	graphics = libvideo_create_driver_context("vesa");
-	if (graphics == NULL)
-	{
-			fprintf(stderr, "Failed to create driver context!\n");
-			exit(EXIT_FAILURE);
-	}
-	libvideo_use_driver_context(graphics);
-	libvideo_get_command_buffer(1024);
-
-	libvideo_change_device(0);
-	
-	// Somehow this is necessary before setting video mode!
-	libvideo_do_command_buffer();
-
-	if (libvideo_change_display_resolution(0, 1024, 768, 24))
-	{
-			printf("Failed to set video mode!\n");
-			exit(EXIT_FAILURE);
-	}
-
-	frontbuffer = libvideo_get_frontbuffer_bitmap(0);
-	libvideo_request_backbuffers(frontbuffer, 2);
-	libvideo_change_target(frontbuffer);
+	return EXIT_SUCCESS;
 }
